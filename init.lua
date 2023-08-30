bedef1c7f72147fc23b7a8f7dddac587c82a62d4
FatalErr42O <58855799+FatalError42O@users.noreply.github.com>
2023-08-30 15:24:42 -0700

    moved modlib and it's properties into a seperate file for licensing reasons

--- init.lua
+++ init.lua
@@ -2,8 +2,11 @@
 mtul.b3d = {}
 
 local modpath = minetest.get_modpath("mtul_b3d")
-dofile(modpath.."/read_b3d.lua")
-dofile(modpath.."/write_b3d.lua")
+--placed in a seperate directory for the license
+dofile(modpath.."/modlib/read_b3d.lua")
+dofile(modpath.."/modlib/write_b3d.lua") --this is untested, could be very broken.
 --these modules are disabled, refactoring is needed.
---dofile(modpath.."/read_b3d_bone.lua")
+if mtul.math.cpml_loaded then
+   dofile(modpath.."/read_b3d_bone")
+end
 --dofile(modpath.."/read_b3d_bone.lua"
